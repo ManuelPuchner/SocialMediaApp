af80ecf86dee586db37a9cbd14945382de65c5d9
ManuelPuchner <75803474+ManuelPuchner@users.noreply.github.com>
2022-01-15 18:58:48 +0100

    (stable): deleting post error now fixed

--- pages/api/deletePost.js
+++ pages/api/deletePost.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
     const posts = await db.collection("posts");
 
     const post = await posts.findOne({});
-    if (user.username !== post.account.user)
+    if (user.username !== post.account.username)
       return res
         .status(401)
         .json({ status: "error", message: "Not Authorized!" });
